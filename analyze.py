cb516ccf24f628232f88611a1e79dc3f258e7ef5
Sushrut Girdhari <developer@girdhari.in>
2015-10-30 06:51:34 -0700

    Sort files and print in descending order according to filesize

--- analyze.py
+++ analyze.py
@@ -1,10 +1,29 @@
 import os
-DIRPATH = os.path.expanduser('~')
+import sys
+import json
+import operator
 
+
+DIRPATH = os.path.expanduser('~')
+DIRPATH = os.path.join(DIRPATH, 'DiskAnalyze')
+
+files = []
+
+print 'Scanning through your disk. Please wait...'
+print 'The process may take up to 20 minutes...'
+
+result = ()
+json_str=""
 for root, directories, filenames in os.walk(DIRPATH):
     for filename in filenames:
         currentfile = os.path.join(root,filename)
         currentstat = os.stat(currentfile)
         currentsize = currentstat.st_size
-        result = [filename, currentsize]
-        print result
+        #tup = (filename, currentsize)
+        tup = (currentsize, filename)
+        files.append(tup)
+
+files.sort(cmp=None,key=None,reverse=True) #Sort files in non-increasing order according to their respective file size
+
+for f in files:
+    print f
